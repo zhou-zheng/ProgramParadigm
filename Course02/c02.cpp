2e6678eb857c7135d2a49dfa8966d3bf9f1792e5
Alex Zhou <zhou_zheng_cn@126.com>
2018-06-26 17:33:37 +0800

    modify Course02

--- Course02/c02.cpp
+++ Course02/c02.cpp
@@ -9,7 +9,8 @@ int main(int argc, char* argv[])
 	short s = *(short *)&f;
 	unsigned char *pf = (unsigned char *)&f;
 
-	printf("%p=%02x,%p=%02x,%p=%02x,%p=%02x\n", &pf[0], pf[0], &pf[1], pf[1], &pf[2], pf[2], &pf[3], pf[3]);
+	printf("%p=%02x\n%p=%02x\n%p=%02x\n%p=%02x\n", 
+		&pf[0], pf[0], &pf[1], pf[1], &pf[2], pf[2], &pf[3], pf[3]);
 	printf("f = %f(%p), s = %d(%p)\n", f, &f, s, &s);
 	
     return 0;
